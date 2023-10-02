d868a253206b66966f2e6cd372e7b792d412093e
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2023-10-02 21:46:05 +0200

    update CORS headers

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://lazycup.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
