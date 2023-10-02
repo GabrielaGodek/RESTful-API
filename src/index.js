d7541a57b0fafe3a88863008ec5d45bcb070f7a1
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2023-10-02 21:15:33 +0200

    Update CORS headers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import coffeesRoutes from "../routes/coffeesRoutes.js";
 import usersRoutes from "../routes/usersRoutes.js";
 import welcomeRoutes from "../routes/welcomeRoutes.js";
-import compression from "compression"
+import compression from "compression";
 
 const app = express();
 
@@ -11,12 +11,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  req.requestTime = new Date().toDateString();
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
-app.use(compression())
+app.use(compression());
 
-app.use('/', welcomeRoutes)
+app.use("/", welcomeRoutes);
 app.use("/api/v1/users", usersRoutes);
 app.use("/api/v1/coffees", coffeesRoutes);
 
